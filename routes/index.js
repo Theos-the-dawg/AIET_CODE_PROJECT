b6684437ef2e137434ba42b8a3ae7468f5fb8bdd
valencycodecussler <geraldkapurura07@gmail.com>
2026-05-10 20:43:24 +0200

    updartes

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,30 @@ const express = require('express');
 const router = express.Router();
 
 router.use(express.json())
+/* GET home page. */
+// Home Page 
+//  router.get('/home', (req,res) => {
+//   console.log('Hi');
+//   res.send('This is the home page')
+//  });
+  
+// // About Us Page
+// router.get('/about', (req, res) => {
+//   res.send('<h1>About Us</h1><p>This is the About section of our router.</p>');
+// });
+
+// //  Contact Us Page
+// router.get('/contact', (req, res) => {
+//   res.send('<h1>Contact Us</h1><p>Reach out to us here.</p>');
+// });
 
 //IndexRoutes
-router.get('/', (req, res) => res.render('home', { title: 'Home', message: 'Welcome to Gadget-House.' }));
-router.get('/about', (req, res) => res.render('about', { title: 'About Us' }));
-router.get('/contact', (req, res) => res.render('contact', { title: 'Contact Us' }));
-router.get('/services', (req, res) => res.render('services', { title: 'Our Services' }));
+router.get('/',(req,res) => res.send('Welcome to Gagdet-House.'));
+router.get('/about',(req,res) => res.send('This is the our About page.'));
+router.get('/contact',(req,res) => res.send('This is our Contact page.'));
+router.get('/services',(req,res) => res.send('This is our Services page.'));
+router.get('/logout',(req,res) => res.send('This is the Logout page.'));
+
 
 
 module.exports = router;
